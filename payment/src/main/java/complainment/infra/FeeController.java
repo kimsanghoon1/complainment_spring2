3b6395ded88561ce6dff43de62afef0b3b387e4b
Samuni <sanghoon01@uengine.org>
2024-11-01 15:12:49 +0900

    commit

--- payment/src/main/java/complainment/infra/FeeController.java
+++ payment/src/main/java/complainment/infra/FeeController.java
@@ -1,27 +1,38 @@
 package complainment.infra;
 
-import complainment.domain.*;
-import java.util.Optional;
+import complainment.domain.Fee;
+import complainment.domain.FeeRepository;
+import complainment.domain.PayCommand;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-//<<< Clean Arch / Inbound Adaptor
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-// @RequestMapping(value="/fees")
-@Transactional
 public class FeeController {
 
     @Autowired
     FeeRepository feeRepository;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping(
-        value = "/fees/{id}/pay",
+        value = "fees/{id}/pay",
         method = RequestMethod.PUT,
         produces = "application/json;charset=UTF-8"
     )
@@ -42,4 +53,3 @@ public class FeeController {
         return fee;
     }
 }
-//>>> Clean Arch / Inbound Adaptor
--- receipt/src/main/java/complainment/infra/ComplainmentController.java
+++ receipt/src/main/java/complainment/infra/ComplainmentController.java
@@ -1,23 +1,58 @@
 package complainment.infra;
 
-import complainment.domain.*;
-import java.util.Optional;
+import complainment.domain.Complainment;
+import complainment.domain.ComplainmentRepository;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-//<<< Clean Arch / Inbound Adaptor
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-// @RequestMapping(value="/complainments")
-@Transactional
 public class ComplainmentController {
 
     @Autowired
     ComplainmentRepository complainmentRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${api.url.fee}")
+    private String apiUrl;
+
+    @Value("${api.url.fee}")
+    private String apiUrl;
+
+    @GetMapping("/complainment/validateFee/{id}")
+    public ResponseEntity<String> feeStockCheck(
+        @PathVariable(value = "id") Long id
+    ) {
+        String complaintReceivedUrl = apiUrl + "/complaintReceiveds/" + id;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+
+        ResponseEntity<String> complaintReceivedEntity = restTemplate.exchange(
+            complaintReceivedUrl,
+            HttpMethod.GET,
+            entity,
+            String.class
+        );
+
+        return complaintReceivedEntity;
+    }
 }
-//>>> Clean Arch / Inbound Adaptor
